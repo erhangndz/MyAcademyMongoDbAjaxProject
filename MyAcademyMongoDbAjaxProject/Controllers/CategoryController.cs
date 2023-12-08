280741ffd9b7725ef49ea43332d0ab298456d7a1
Erhan Gündüz <erhangndz@live.com>
2023-12-08 21:35:36 +0300

    Add project files.

---
+++ MyAcademyMongoDbAjaxProject/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MyAcademyMongoDbAjaxProject.DAL.Entities;
+using MyAcademyMongoDbAjaxProject.DAL.Settings;
+
+namespace MyAcademyMongoDbAjaxProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IMongoCollection<Category> _categoryCollection;
+     
+
+        public CategoryController(IMongoCollection<Category> categoryCollection, IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            _categoryCollection = database.GetCollection<Category>(databaseSettings.CategoryCollectionName);
+            
+        }
+
+        public IActionResult Index()
+        {
+            var values = _categoryCollection.AsQueryable().ToList();
+            return View();
+        }
+    }
+}
---
+++ MyAcademyMongoDbAjaxProject/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MyAcademyMongoDbAjaxProject.DAL.Entities;
+using MyAcademyMongoDbAjaxProject.DAL.Settings;
+
+namespace MyAcademyMongoDbAjaxProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IMongoCollection<Product> _productCollection;
+
+        public ProductController(IMongoCollection<Product> productCollection,IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            _productCollection = database.GetCollection<Product>(databaseSettings.ProductCollectionName);
+
+
+            _productCollection = productCollection;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
